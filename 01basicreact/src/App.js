9e9cd20d6ff0a5e21e0e6fc20a42d9dcaf61f794
Rakhitha Dharmasiri <rakhithaprabashwara@gmail.com>
2024-08-24 23:15:23 +0530

    01basic done

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Classic react app  </h1>
+  );
+}
+
+export default App;
---
+++ 01basicvite/src/Youtube.jsx
@@ -0,0 +1,8 @@
+function Youtube(){
+    return(
+        <h1>Hello</h1>
+        
+    )
+}
+
+export default Youtube
