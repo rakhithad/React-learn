6ac73739cfba10c6cc1d262589e74c52d2cc5222
Rakhitha Dharmasiri <rakhithaprabashwara@gmail.com>
2024-09-01 22:49:47 +0530

    02counter done

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [counter, setCounter]= useState(15)
+  //let counter = 15
 
+  const addValue = () => {
+    setCounter(
+      counter + 1
+    )
+  }
+  const removeValue = () => {
+    setCounter(counter-1)
+  }
   return (
     <>
-      <h1>React course with rakhitha</h1>
+      <h1>React course with rakhitha {counter}</h1>
+      <h2>counter value: {counter}</h2>
+      <button onClick={addValue}>Add value</button> {" "}
+      <button onClick={removeValue}>remove value</button>
+      <p>footer: {counter}</p>
     </>
   )
 }
